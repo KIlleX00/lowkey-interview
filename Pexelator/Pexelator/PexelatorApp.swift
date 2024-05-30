6567bdf2fc3c24dc926fda7fd5a81ec043d5db27
Marek Sokol <marek92sokol@gmail.com>
2024-05-30 20:16:13 +0200

    Project setup.

---
+++ Pexelator/Pexelator/PexelatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelatorApp.swift
+//  Pexelator
+//
+//  Created by Marek Sokol on 30.5.24..
+//
+
+import SwiftUI
+
+@main
+struct PexelatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
